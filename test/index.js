4df0ddef51d384ea3e7eb9b228e6104204e267a3
ismay <ismay@ismaywolff.nl>
2015-07-16 20:56:01 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -2,12 +2,12 @@
 var assert = require('assert');
 var equal = require('assert-dir-equal');
 var Metalsmith = require('metalsmith');
-var templates = require('..');
+var layouts = require('..');
 
-describe('metalsmith-templates', function(){
+describe('metalsmith-layouts', function(){
   it('should render a basic template', function(done){
     Metalsmith('test/fixtures/basic')
-      .use(templates({ engine: 'swig' }))
+      .use(layouts({ engine: 'swig' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
@@ -17,7 +17,7 @@ describe('metalsmith-templates', function(){
 
   it('should accept an engine string', function(done){
     Metalsmith('test/fixtures/basic')
-      .use(templates('swig'))
+      .use(layouts('swig'))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
@@ -25,19 +25,9 @@ describe('metalsmith-templates', function(){
       });
   });
 
-  it('should accept an inPlace option', function(done){
-    Metalsmith('test/fixtures/in-place')
-      .use(templates({ engine: 'swig', inPlace: true }))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/in-place/expected', 'test/fixtures/in-place/build');
-        done();
-      });
-  });
-
   it('should accept a pattern to match', function(done){
     Metalsmith('test/fixtures/pattern')
-      .use(templates({ engine: 'swig', pattern: '*.md' }))
+      .use(layouts({ engine: 'swig', pattern: '*.md' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/pattern/expected', 'test/fixtures/pattern/build');
@@ -47,7 +37,7 @@ describe('metalsmith-templates', function(){
 
   it('should accept a default template', function(done){
     Metalsmith('test/fixtures/default')
-      .use(templates({ engine: 'swig', pattern: '*.md', default: 'default.html' }))
+      .use(layouts({ engine: 'swig', pattern: '*.md', default: 'default.html' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/pattern/expected', 'test/fixtures/pattern/build');
@@ -55,9 +45,9 @@ describe('metalsmith-templates', function(){
       });
   });
 
-  it('should accept a different templates directory', function(done){
+  it('should accept a different layouts directory', function(done){
     Metalsmith('test/fixtures/directory')
-      .use(templates({ engine: 'swig', directory: 'layouts' }))
+      .use(layouts({ engine: 'swig', directory: 'templates' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/directory/expected', 'test/fixtures/directory/build');
@@ -68,7 +58,7 @@ describe('metalsmith-templates', function(){
   it('should mix in global metadata', function(done){
     Metalsmith('test/fixtures/metadata')
       .metadata({ title: 'Global Title' })
-      .use(templates({ engine: 'swig' }))
+      .use(layouts({ engine: 'swig' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/metadata/expected', 'test/fixtures/metadata/build');
@@ -78,7 +68,7 @@ describe('metalsmith-templates', function(){
 
   it('should preserve binary files', function(done){
     Metalsmith('test/fixtures/binary')
-      .use(templates({ engine: 'swig' }))
+      .use(layouts({ engine: 'swig' }))
       .build(function(err){
         if (err) return done(err);
         equal('test/fixtures/binary/expected', 'test/fixtures/binary/build');
